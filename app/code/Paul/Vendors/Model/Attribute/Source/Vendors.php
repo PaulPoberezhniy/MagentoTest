a25ed4fcd9b3aa762b57f7c75b77fb3e700746a0
PaulPoberezhniy <pavlopobereznyi@gmail.com>
2018-10-04 21:55:12 +0300

    added vendor attribute for product

---
+++ app/code/Paul/Vendors/Model/Attribute/Source/Vendors.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Paul\Vendors\Model\Attribute\Source;
+use Magento\Eav\Model\Entity\Attribute\Source\AbstractSource;
+
+class Vendors extends AbstractSource
+{
+    /**
+    * @var \Paul\Vendors\Model\Vendors
+    */
+    protected $_model;
+    /**
+    * Vendor constructor.
+    * @param \Paul\Vendors\Model\Vendors $model
+    */
+    public function __construct(\Paul\Vendors\Model\Vendors $model)
+    {
+        $this->_model = $model;
+    }
+    /**
+    * Create options array
+    *
+    * @return array
+    */
+    public function getAllOptions()
+    {
+        $options = [];
+        $modelCollection = $this->_model->getCollection()
+        ->addFieldToSelect('entity_id')
+        ->addFieldToSelect('name');
+        foreach ($modelCollection as $item){
+        $options[] = [
+            'label' => $item->getName(),
+            'value' => $item->getId(),
+        ];
+        }
+        return $options;
+    }
+}
---
+++ app/code/Paul/Vendors/Setup/InstallData.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Paul\Vendors\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * Eav setup factory
+     * @var \Magento\Eav\Setup\EavSetupFactory
+     */
+    private $eavSetupFactory;
+    /**
+     * InstallData constructor.
+     * @param \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory) {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create();
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'my_vendor',
+            [
+                'group' => 'Vendor group',
+                'type' => 'varchar',
+                'backend' => '',
+                'fronend' => '',
+                'label' => 'My Vendor',
+                'input' => 'select',
+                'note' => 'My Vendor',
+                'class' => '',
+                'source' => 'Paul\Vendors\Model\Attribute\Source\Vendors',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => true,
+                'used_in_product_listing' => true,
+                'unique' => false,
+            ]
+        );
+    }
+}
