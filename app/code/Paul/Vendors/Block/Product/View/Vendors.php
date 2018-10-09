344d978071ede9d0653ea2804418eb23613e2e9d
PaulPoberezhniy <pavlopobereznyi@gmail.com>
2018-10-09 21:32:29 +0300

    vendor logo after product sku

--- app/code/Paul/Vendors/Block/Product/View/Vendors.php
+++ app/code/Paul/Vendors/Block/Product/View/Vendors.php
@@ -6,5 +6,34 @@ use Magento\Catalog\Block\Product\AbstractProduct;
 
 class Vendors extends AbstractProduct
 {
+    protected $_vendor;
 
+    /**
+     * @param \Magento\Catalog\Block\Product\Context$context
+     * @param \Paul\Vendors\Model\Vendors $vendor,
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        \Paul\Vendors\Model\Vendors $vendor,
+        array $data = []
+    ) {
+        $this->_vendor = $vendor;
+
+        parent::__construct(
+            $context,
+            $data
+        );
+    }
+
+    protected function _getVendor($entityId)
+    {
+        $item = $this->_vendor->load($entityId);
+        return ($item->getData());
+    }
+
+    public function getLoadedVendor($entityId)
+    {
+        return $this->_getVendor($entityId);
+    }
 }
